53ac0f03d868b458c74d0298f1042ca4ea971710
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-11-18 18:16:51 +0600

    final

--- solution.ts
+++ solution.ts
@@ -34,12 +34,6 @@ class Person {
   }
 }
 
-const person1 = new Person("John Doe", 30);
-console.log(person1.getDetails());
-
-const person2 = new Person("Alice", 25);
-console.log(person2.getDetails());
-
 const filterByRating = (
   products: { title: string; rating: number }[]
 ): object[] => {
